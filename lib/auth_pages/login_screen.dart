441749ca64cd817945d7413896f8f8db16b5cf13
Sanya0909 <skuckarov8@gmail.com>
2022-02-06 19:31:31 +0500

    initial

---
+++ lib/auth_pages/login_screen.dart
@@ -0,0 +1,120 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/painting.dart';
+import 'package:flutter/services.dart';
+
+class LoginScreen extends StatefulWidget {
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  final _phoneController = TextEditingController();
+
+  bool isHidePassword = true;
+
+  @override
+  Widget build(BuildContext context) {
+    double screenHeight = MediaQuery.of(context).size.height;
+    double screenWidth = MediaQuery.of(context).size.width;
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.only(top: screenHeight * 0.17),
+          child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Center(
+                  child: Text(
+                    'Установите пароль',
+                    style: TextStyle(
+                      fontWeight: FontWeight.w600,
+                      fontSize: 18,
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 40,
+                ),
+                Padding(
+                  padding: EdgeInsets.only(right: screenWidth * 0.47, left: 45),
+                  child: Text(
+                    "Ваш номер телефона",
+                    style: TextStyle(
+                        color: Colors.grey,
+                        fontSize: 14,
+                        fontWeight: FontWeight.w600),
+                  ),
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 40, right: 40),
+                  child: TextField(
+                    decoration: InputDecoration(border: OutlineInputBorder()),
+                    keyboardType: TextInputType.number,
+                    controller: _phoneController,
+                  ),
+                ),
+                SizedBox(
+                  height: 13,
+                ),
+                Padding(
+                  padding: EdgeInsets.only(left: 45, right: screenWidth * 0.73),
+                  child: Text('Пароль',
+                      style: TextStyle(
+                          fontSize: 14,
+                          fontWeight: FontWeight.w600,
+                          color: Colors.grey)),
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(right: 40, left: 40),
+                  child: TextField(
+                    obscureText: isHidePassword,
+                    decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        suffixIcon: InkWell(
+                            onTap: _togglePasswordView,
+                            child: Icon(Icons.visibility))),
+                  ),
+                ),
+                SizedBox(
+                  height: 30,
+                ),
+                ElevatedButton(
+                  onPressed: () {},
+                  child: Text('Установить пароль'),
+                  style: ElevatedButton.styleFrom(
+                      primary: Colors.deepOrange, fixedSize: Size(280, 45)),
+                ),
+                SizedBox(
+                  height: 25,
+                ),
+                ElevatedButton(
+                  onPressed: () {},
+                  child: Text(
+                    'Пропустить',
+                    style: TextStyle(
+                        color: Colors.deepOrange,
+                        fontSize: 18,
+                        fontWeight: FontWeight.w600),
+                  ),
+                  style: ElevatedButton.styleFrom(
+                      primary: Colors.transparent, elevation: 0),
+                )
+              ]),
+        ),
+      ),
+    );
+  }
+
+  void _togglePasswordView() {
+    setState(() {
+      isHidePassword = !isHidePassword;
+    });
+  }
+}
